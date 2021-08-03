c1a7b0b7a1503c851d94a712c81090ae86815d41
flashy86 <annecharlotte.lucas2021@campus-eni.fr>
2021-08-03 17:16:24 +0200

    Injection de datas et hydratation d'entity

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,6 +39,44 @@ class SerieController extends AbstractController
 	 */
 	public function create(Request $request):Response{
 		dump($request);
+		return $this->render('series/create.html.twig', [
+
+		]);
+	}
+	/**
+	 * @Route ("/series/demo", name="serie_em-demo")
+	 */
+	public function demo(EntityManagerInterface $entityManager):Response{
+		//démo pour entitymanager
+		//création d'une instance de l'entité
+		$serie = new Serie();
+
+		//hydrater toutes les propriétés
+		$serie->setName('pif');
+		$serie->setBackdrop('dafsd');
+		$serie->setPoster('dafsdf');
+		$serie->setDateCreated(new \DateTime());
+		$serie->setFirstAirDate(new \DateTime("-1year"));
+		$serie->setLastAirDate(new  \DateTime("-6 month"));
+		$serie->setGenres('drama');
+		$serie->setOverview('bla bla bla');
+		$serie->setPopularity(123.00);
+		$serie->setVote(8.2);
+		$serie->setStatus('cancelled');
+		$serie->setTmdbId(329432);
+
+		dump($serie);
+
+		$entityManager->persist($serie);
+		$entityManager->flush();
+
+		dump($serie);
+
+		$serie->setGenres('comedy');
+
+		//$entityManager->remove($serie);
+		$entityManager->flush();
+
 		return $this->render('series/create.html.twig', [
 
 		]);
